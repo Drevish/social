d8f5cdba9ef36a5998796779ee9dac03801649ac
Drevish <vladimirnemolovsky@gmail.com>
2019-09-16 16:32:11 +0300

    Tests for EditController

--- src/main/java/com/drevish/social/controller/dto/UserInfo.java
+++ src/main/java/com/drevish/social/controller/dto/UserInfo.java
@@ -2,9 +2,13 @@ package com.drevish.social.controller.dto;
 
 import com.drevish.social.anno.Name;
 import com.drevish.social.anno.Surname;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserInfo {
     @Name
     private String name;
