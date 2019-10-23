e1bb25e388a07ebcab8c57e9a2400ba943d479cf
Drevish <vladimirnemolovsky@gmail.com>
2019-10-23 15:38:05 +0300

    Friend features on user profile page

--- src/main/java/com/drevish/social/controller/ProfileController.java
+++ src/main/java/com/drevish/social/controller/ProfileController.java
@@ -2,9 +2,12 @@ package com.drevish.social.controller;
 
 import com.drevish.social.exception.InvalidImageException;
 import com.drevish.social.exception.UserNotFoundException;
+import com.drevish.social.model.entity.FriendState;
 import com.drevish.social.model.entity.User;
 import com.drevish.social.model.entity.UserInfo;
+import com.drevish.social.service.FriendService;
 import com.drevish.social.service.UserService;
+import com.drevish.social.util.BiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +21,7 @@ import java.security.Principal;
 @Controller
 @Slf4j
 @RequestMapping("/profile")
+//TODO: test friend methods
 public class ProfileController extends ControllerWithUserInfo {
     @Value("${view.profile}")
     private String profileView;
@@ -30,6 +34,9 @@ public class ProfileController extends ControllerWithUserInfo {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private FriendService friendService;
 
     @GetMapping
     public String showOwnProfile(Principal principal, Model model) {
@@ -54,7 +61,47 @@ public class ProfileController extends ControllerWithUserInfo {
         UserInfo userInfo = userInfoService.getUserInfoByEmail(user.getEmail());
         model.addAttribute("otherUserInfo", userInfo);
 
+        User current = userService.getUserByEmail(principal.getName());
+        FriendState relation = friendService.getRelation(current, user);
+        model.addAttribute("relation", relation);
+
         return otherProfileView;
+    }
+
+    @PostMapping(value = "/id{userId}", params = "change=subscribe")
+    public String subscribe(@PathVariable Long userId, Principal principal) {
+        return friendMethod(FriendState.NONE, (t, r) -> friendService.subscribe(t, r),
+                userId, principal);
+    }
+
+    @PostMapping(value = "/id{userId}", params = "change=unsubscribe")
+    public String unsubscribe(@PathVariable Long userId, Principal principal) {
+        return friendMethod(FriendState.UPCOMING_FRIEND_REQUEST, (t, r) -> friendService.unsubscribe(t, r),
+                userId, principal);
+    }
+
+    @PostMapping(value = "/id{userId}", params = "change=acceptFriendRequest")
+    public String acceptFriendRequest(@PathVariable Long userId, Principal principal) {
+        return friendMethod(FriendState.INCOMING_FRIEND_REQUEST, (t, r) -> friendService.acceptFriendRequest(t, r),
+                userId, principal);
+    }
+
+    @PostMapping(value = "/id{userId}", params = "change=deleteFriend")
+    public String deleteFriend(@PathVariable Long userId, Principal principal) {
+        return friendMethod(FriendState.FRIEND, (t, r) -> friendService.deleteFriend(t, r),
+                userId, principal);
+    }
+
+    private String friendMethod(FriendState requiredRelation, BiOperation<User, User> operator,
+                                Long userId, Principal principal) {
+        User another = userService.getUserById(userId);
+        User user = userService.getUserByEmail(principal.getName());
+
+        FriendState relation = friendService.getRelation(user, another);
+        if (relation.equals(requiredRelation)) {
+            operator.run(user, another);
+        }
+        return "redirect:/profile/id" + userId;
     }
 
     @PostMapping(params = "change=upload-image")
---
+++ src/main/java/com/drevish/social/util/BiOperation.java
@@ -0,0 +1,5 @@
+package com.drevish.social.util;
+
+public interface BiOperation<T, R> {
+    void run(T op1, R op2);
+}
